edd35a1effda6ce2f3c2301cc7df45b7c2d1714d
deca7777 <83956600+deca7777@users.noreply.github.com>
2022-08-15 11:51:51 +0900

    Update onRespawn.java

--- src/main/java/com/deca7777/randomspawn/event/onRespawn.java
+++ src/main/java/com/deca7777/randomspawn/event/onRespawn.java
@@ -15,14 +15,7 @@ public class onRespawn implements Listener {
     public void onRespawn(PlayerRespawnEvent e) {
         Player player = e.getPlayer();
         if(!e.isBedSpawn()&&!e.isAnchorSpawn()){
-            FileConfiguration config = plugin.getConfig();
-            randomteleport ra = new randomteleport();
-            ra.center_x = config.getInt("Center-X");
-            ra.center_z = config.getInt("Center-Z");
-            ra.radius = config.getInt("Radius");
-            ra.worldname = config.getString("world");
-            player.teleport(ra.set_pos());
-            player.sendMessage("Teleported to random position...");
+            Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(),"rt "+e.getPlayer().getName());
         }
     }
 }
