7f8364ae1b77fb7ea984758ac83d273fe86cf556
deca7777 <83956600+deca7777@users.noreply.github.com>
2022-08-15 18:59:16 +0900

    Update Main.java

--- src/main/java/com/deca7777/randomspawn/Main.java
+++ src/main/java/com/deca7777/randomspawn/Main.java
@@ -3,13 +3,9 @@ package com.deca7777.randomspawn;
 import com.deca7777.randomspawn.commands.rt;
 import com.deca7777.randomspawn.commands.setcenter;
 import com.deca7777.randomspawn.commands.setradius;
-import com.deca7777.randomspawn.event.onRespawn;
-import com.deca7777.randomspawn.event.onJoin;
-import org.bukkit.entity.Player;
+import com.deca7777.randomspawn.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 
@@ -20,8 +16,7 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		plugin = this;
 		saveResource("config.yml", false);
-		getServer().getPluginManager().registerEvents(new onRespawn(), this);
-		getServer().getPluginManager().registerEvents(new onJoin(), this);
+		getServer().getPluginManager().registerEvents(new Listener(), this);
 		this.getCommand("rt").setExecutor(new rt());
 		this.getCommand("setcenter").setExecutor(new setcenter());
 		this.getCommand("setradius").setExecutor(new setradius());
